550ac000d4c119e0ae1f9b4bd78b0d9481b5579c
kartheek <kartheekchadaram12@gmail.com>
2024-10-23 18:26:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "rahull"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-095ddd20f2a206be7"]
 tags = {
 Name = var.instance_names[count.index]
 }
